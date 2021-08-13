3723cc72d59b899e2df1cfe304490a1dd9d922b8
Ash Barsov <a89875239469@gmail.com>
2021-08-13 22:25:46 +0300

    --add 5td lesson --add react-router-dom

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink, Route, Redirect} from "react-router-dom";
+import './main.module.css'
+import s from './main.module.css';
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <Links />
         </div>
     )
 }
+const Links = () =>  {
+    const [open, setOpen] = useState(false)
+    const handleClick = () => {
+        setOpen(!open);
+    }
+
+
+        return (
+            <pre className={s.link}>
+                <div className={s.square}onClick={handleClick}></div>
+                <div className={`${s.menu} ${ open ? s.open : ''}`}>
+                    <ul>
+                        <li><NavLink onClick={handleClick} to={'/pre-junior'}>Pre Junior</NavLink></li>
+                        <li><NavLink onClick={handleClick} to={'/junior'}>Junior</NavLink></li>
+                        <li><NavLink onClick={handleClick} to={'/juniorPLus'}>Junior PLus</NavLink></li>
+                    </ul>
+                </div>
+            </pre>
+        )
+    }
+
 
 export default Header
--- src/p2-homeworks/h5/pages/PreJunior.tsx
+++ src/p2-homeworks/h5/pages/PreJunior.tsx
@@ -4,20 +4,36 @@ import HW2 from '../../h2/HW2'
 import HW3 from '../../h3/HW3'
 import HW4 from '../../h4/HW4'
 
-function PreJunior() {
-    return (
-        <div>
-            <HW1/>
-            <HW2/>
-            <HW3/>
-            <HW4/>
-            {/*<HW6/>*/}
 
+
+export const PreJunior = function() {
+    return (
+
+            <div>
+                <HW1/>
+                <HW2/>
+                <HW3/>
+                <HW4/>
+            </div>
+    )
+}
+export const Junior = function() {
+    return (
+
+        <div>
+            Comming Soon
+        </div>
+    )
+}
+export const JuniorPlus = function() {
+    return (
+
+        <div>
+            Comming Soon...
         </div>
     )
 }
 
-export default PreJunior
 
 // сделайте по аналогии пустые страницы джун и джун+
 // туда будут добавляться следующие дз
